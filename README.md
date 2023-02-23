aaa7a7e4f3ce29fd151e83952c450ce9b3b91d48
Akhilesh Pandey <akhilesh.pandey@unthinkable.co>
2023-02-23 19:09:30 +0530

    fix: lerna playground added

---
+++ README.md
@@ -0,0 +1,3 @@
+# LERNA Monorepo Config
+## Links
+1. https://javascript.plainenglish.io/managing-javascript-monorepos-with-lerna-building-a-react-icon-library-publishing-to-npm-70855bfb31d9
---
+++ playgrounds/react/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className='Playground'>
+      <h1>Playground</h1>
+      <h2>Test your code here and then publish</h2>
+    </div>
+  )
+}
