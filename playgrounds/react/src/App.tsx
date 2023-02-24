666651c6affc41bbc65459786e6743ef1d05ed20
Akhilesh Pandey <akhilesh.pandey@unthinkable.co>
2023-02-24 12:15:41 +0530

    feat: one svg package added

--- playgrounds/react/src/App.tsx
+++ playgrounds/react/src/App.tsx
@@ -1,11 +1,12 @@
-import React from 'react'
 import "./App.css";
+import LeftArrowSVGIcon from "../../../packages/SvgIcons/LeftArrowIcon";
 
 export default function App() {
   return (
     <div className='Playground'>
       <h1>Playground</h1>
       <h2>Test your code here and then publish</h2>
+      <LeftArrowSVGIcon color={'purple'} width="60px" height="60px"/>
     </div>
   )
 }
