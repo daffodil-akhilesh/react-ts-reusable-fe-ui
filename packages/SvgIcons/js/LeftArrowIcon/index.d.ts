bc9493805637b2d6d884d1aaa4b793e86aaf0716
Akhilesh Pandey <akhilesh.pandey@unthinkable.co>
2023-03-01 19:53:16 +0530

    fix: refactor configs

--- packages/SvgIcons/js/LeftArrowIcon/index.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="react" />
-interface LeftArrowIconProps {
-    color?: string;
-    height?: string;
-    width?: string;
-}
-export default function LeftArrowIcon(props: LeftArrowIconProps): JSX.Element;
-export {};
